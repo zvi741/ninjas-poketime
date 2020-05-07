b6f841c43d8ca54c17962c694abd629e6c6910e9
zvi741 <51421153+zvi741@users.noreply.github.com>
2020-05-07 11:07:48 +0300

    added new components: home, about and contact. add react-router

---
+++ src/Components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate animi inventore unde earum hic nam. Nulla mollitia laboriosam assumenda eos expedita temporibus quibusdam! Vero consequatur dolor voluptate hic ducimus dolore!</p>
+    </div>
+  );
+};
+
+export default Contact;
